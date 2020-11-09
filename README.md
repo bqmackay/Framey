ea11c1fa0ec0500c0e3118ca9c5a9dfc5a2091b0
byronmackay <bqmackay@gmail.com>
2020-11-09 14:23:28 -0700

    Added the ability to add params to the command line call to speed up the process

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Framey
 
-A companion library to [Roomy](https://github.com/jhillman/roomy) that generates a skeleton and best-guess attriubtes of the Roomy json files.
+A companion library to [Roomy](https://github.com/jhillman/roomy) that generates a skeleton and best-guess attributes of the Roomy json files.
 
 ## To Run
-ptyhon3 frame.py
+python3 frame.py <filename.json> <package name> <class name for top layer>
 
-### Prompts
+### Prompts (if params are not given)
 - the path to a json file with an example of the endpoint's response
 - the package name
 - the classname for the top layer of the response
--- frame.py
+++ frame.py
@@ -1,4 +1,11 @@
 import json
+import sys
+
+DEBUG = False
+
+def print_debug(value):
+    if DEBUG:
+        print(value)
 
 #define function that converts an http json response and turns it into roomy json.
 def frame_roomy_from_json_response(json, class_name, package):
@@ -11,37 +18,48 @@ def frame_roomy_from_json_response(json, class_name, package):
     members = []
 
     for key, value in json.items():
+        # print_debug(key)
+
         if key == "success":
             continue
 
         object_dict = {}
         object_dict["name"] = key[0].lower() + key[1:]
-        object_dict["serializedName"] = key[0].upper() + key[1:]
+        object_dict["serializedName"] = key
 
         if isinstance(value, dict):
+            print_debug("%s is a dict: %s" % (key, value))
             object_dict["type"] = "class"
             object_dict["class"] = "com.chatbooks.room.model.*." + key
-            frame_roomy_from_json_response(value, key[0].upper() + key[1:], package)
+            if bool(dict):
+                frame_roomy_from_json_response(value, key[0].upper() + key[1:], package)
 
         if isinstance(value, list):
+            print_debug("%s is a list: %s" % (key, value))
             object_dict["type"] = "class[]"
             object_dict["class"] = "com.chatbooks.room.model.*." + key[0:-1]
-            frame_roomy_from_json_response(value[0], key[0].upper() + key[1:], package)
+            new_key = key[0].upper() + key[1:]
+            if len(value) > 0 and isinstance(value[0], dict):
+                frame_roomy_from_json_response(value[0], new_key, package)
+
+        elif isinstance(value, bool):
+            print_debug("%s is a bool: %s" % (key, value))
+            object_dict["class"] = "kotlin.Boolean"
 
         elif isinstance(value, int):
             if key.endswith("id"):
+                print_debug("%s is a long: %s" % (key, value))
                 object_dict["class"] = "kotlin.Long"
             else:
+                print_debug("%s is a int: %s" % (key, value))
                 object_dict["class"] = "kotlin.Int"
 
         elif isinstance(value, str):
+            print_debug("%s is a string: %s" % (key, value))
             object_dict["class"] = "kotlin.String"
 
-        elif isinstance(value, bool):
-            object_dict["class"] = "kotlin.Boolean"
-
         else:
-            print("Couldn't find type for " + key)
+            print("Couldn't find type for %s" % key)
 
         members.append(object_dict)
 
@@ -51,18 +69,29 @@ def frame_roomy_from_json_response(json, class_name, package):
 
     output.insert(0, top_object)
 
-path = input("Path to response file: ")
+if len(sys.argv) < 2:
+    path = input("Path to response file: ")
+else:
+    path = sys.argv[1]
 # path = "sub.json"
 file = open(path,)
 data = json.load(file)
 
-package = input("What package will this go into? ")
+if len(sys.argv) < 3:
+    package = input("What package will this go into? ")
+else:
+    package = sys.argv[2]
+
 # package = "groups"
 
 # baseclass = input ("what basclass will the top object be using? ")
 # baseclass = "com.chatbooks.room.model.common.SimpleResponse"
 
-top_class_name = input ("what is the name of the top object? ")
+if len(sys.argv) < 4:
+    top_class_name = input ("what is the name of the top object? ")
+else:
+    top_class_name = sys.argv[3]
+
 # top_class_name = "ActiveSubscriptionsResponse"
 
 output = []
